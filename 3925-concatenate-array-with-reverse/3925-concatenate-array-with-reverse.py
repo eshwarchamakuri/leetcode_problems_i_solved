b4c5e0d1a6ded9bee1f5c8e8033c8cad2cf5b323
eshwarchamakuri <eshwarchamakuri@gmail.com>
2026-09-18 11:41:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.19%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.py
@@ -1,6 +1,4 @@
 class Solution:
     def concatWithReverse(self, nums: list[int]) -> list[int]:
-        original=nums
-        reverse=nums[::-1]
-        total=original+reverse
-        return total
+        
+        return nums+nums[::-1]
