2217fd386239f9c4c9a14a92f1e03a00df9ea399
eshwarchamakuri <eshwarchamakuri@gmail.com>
2026-09-18 11:46:25 +0530

    Time: 0 ms (100%), Space: 19.1 MB (88.25%) - LeetHub

---
+++ 3936-minimum-swaps-to-move-zeros-to-end/3936-minimum-swaps-to-move-zeros-to-end.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumSwaps(self, nums: list[int]) -> int:
+        i=0
+        j=len(nums)-1
+        count=0
+        while i<j:
+            if nums[j]==0:
+                j-=1
+            elif nums[i]==0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+                count+=1
+            else:
+                i+=1
+        return count
