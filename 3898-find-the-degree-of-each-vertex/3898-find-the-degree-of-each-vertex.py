e8cfcd6aac6ea6fef4af5a7008a25d03cd00883e
eshwarchamakuri <eshwarchamakuri@gmail.com>
2026-09-20 16:26:48 +0530

    Time: 4 ms (74.13%), Space: 20 MB (61.86%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.py
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.py
@@ -1,6 +1,3 @@
 class Solution:
     def findDegrees(self, matrix: list[list[int]]) -> list[int]:
-        result=[]
-        for i in matrix:
-            result.append(sum(i))
-        return result
+        return [sum(matrix[mat]) for mat in range(len(matrix))]
